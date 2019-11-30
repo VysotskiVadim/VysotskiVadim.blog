fd42ffc29754246b5109cba106f28dceb1626209
vadzimv <vysotski.vadim@gmail.com>
2019-11-30 17:21:46 +0300

    reduces image quality

--- resize-images.sh
+++ resize-images.sh
@@ -2,6 +2,6 @@ for fullFileName in ./assets/*.jpg; do
     fileName=$(basename $fullFileName .jpg)
     echo "converting ${fileName}"
     for width in 1920 1600 1366 1024 768 640; do
-        convert $fullFileName -resize ${width}x "./assets/resized/${fileName}_${width}.jpg"
+        convert $fullFileName -resize ${width}x -quality 85% "./assets/resized/${fileName}_${width}.jpg"
     done 
 done
